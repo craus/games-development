b1f9cf648ef66711c55dc524649e82a8a3fed770
craus <craus902@gmail.com>
2014-05-19 18:29:13 +0400

    det magic duel!

---
+++ det_magic_duel.js
@@ -0,0 +1,23 @@
+function start() {
+	return {
+		players: [
+			{
+				creatures: [],
+				mana: 0,
+				hp: 12
+			},
+			{
+				creatures: [],
+				mana: 1,
+				hp: 12
+			}
+		],
+		creaturesMoved: 0
+	}
+}
+
+function firstWins(game) {
+
+}
+
+console.log(start())
